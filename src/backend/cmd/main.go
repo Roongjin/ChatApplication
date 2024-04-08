414012df7c326732ef5ce28dae8fd90745c30f76
Roongkun <roongkun.l@gmail.com>
2024-04-08 19:57:06 +0700

    Fixed gitignore

--- src/backend/cmd/main.go
+++ src/backend/cmd/main.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"log"
 
-	"github.com/Roongjin/ChatApplication/src/backend/internal/migrate"
+	"github.com/Roongjin/ChatApplication/src/backend/internal/cli/migrate"
 )
 
 func main() {
