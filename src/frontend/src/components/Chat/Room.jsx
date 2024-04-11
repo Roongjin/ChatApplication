cf136a7fcf280199ccf9f883ac8ac4d0a3a0ae9f
Roongkun <roongkun.l@gmail.com>
2024-04-11 16:54:15 +0700

    Converted timestamptz to h:m

--- src/frontend/src/components/Chat/Room.jsx
+++ src/frontend/src/components/Chat/Room.jsx
@@ -1,16 +1,15 @@
 import "@/index.css";
 
 const Message = ({ message }) => {
+  const time = message.ts.match(/\d\d:\d\d/);
   return (
-    <div className="flex items-start p-5">
+    <div className="flex w-max items-start p-5 bg-gray-50 border border-gray-300 rounded-lg">
       <div className="flex flex-col w-full max-w-[320px] leading-1.5">
         <div className="flex items-center space-x-2 rtl:space-x-reverse">
           <span className="text-sm font-semibold text-gray-900">
             {message.sender_name}
           </span>
-          <span className="text-sm font-normal text-gray-500">
-            {message.ts}
-          </span>
+          <span className="text-sm font-normal text-gray-500">{time}</span>
         </div>
         <p className="text-sm font-normal py-2 text-gray-900">{message.data}</p>
         <span className="text-sm font-normal text-gray-500">Delivered</span>
